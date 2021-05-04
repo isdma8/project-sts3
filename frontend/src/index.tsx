6e098cd9bc0b8b1128547fd5b65a926a4c6ad9bf
Daniel Almeida <daniel.mag.almeida@gmail.com>
2021-05-04 15:47:58 +0100

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
 import App from './App';
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
